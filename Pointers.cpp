1894018a1234cd829f0d7b2629befbd672abf28d
Amruta Prashant Gotmare <gotmareamruta01@gmail.com>
2026-07-07 20:31:17 +0530

    Create Pointers.cpp

---
+++ Pointers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=4;
+    int*b = &a;
+    cout<<"The address of a is: "<<&a<<endl;
+    cout<<"The address of a is: "<<b<<endl;
+    cout<<"The value of a is: "<<*b<<endl;
+
+    //Pointer to pointer
+    int**c = &b;
+    cout<<"The address of b is: "<<c<<endl;
+    cout<<"The value of b  at address c is: "<<*c<<endl;
+    cout<<"The value  at address value_at (value_at (c)) is: "<<**c<<endl;
+
+    return 0;
+}
